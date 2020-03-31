5898de2c63f9297a0f358970b03b225fa4e49a07
benjym <benjy.marks@sydney.edu.au>
2020-04-01 10:09:58 +1100

    Generalised regular grid definition for arbitrary grid spacings and sizes

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,9 @@
-function add_road_segment(pts) {
-    var L = Math.abs(pts[1][0]-pts[0][0]);
-    var H = Math.abs(pts[1][1]-pts[0][1]);
-    if (L > 0) { L += 1; }
-    if (H > 0) { H += 1; }
+function add_road_segment(pts,R) {
+    var L = Math.abs(pts[1][0]-pts[0][0]) + 2*R;
+    var H = Math.abs(pts[1][1]-pts[0][1]) + 2*R;
+    // if (L > 0) { L += 2*R; }
+    // if (H > 0) { H += 2*R; }
+    // console.log(L,H);
     var geometry = new THREE.PlaneGeometry( L, H );
     var road = new THREE.Mesh( geometry, road_material );
     road.position.x = (pts[1][0]+pts[0][0])/2.;
@@ -52,9 +53,9 @@ function add_line_marking_segment(pts) {
     scene.add( line );
 }
 
-function add_road_network(links) {
+function add_road_network(links,R) {
     links.forEach(function(pts, index, array) {
-        add_road_segment(pts);
+        add_road_segment(pts,R);
     });
     links.forEach(function(pts, index, array) {
         add_line_marking_segment(pts);
@@ -84,6 +85,34 @@ function locate_domain() {
     }
 }
 
+function generate_regular_roads(W,H,R,B) {
+    // W = 25 // half width in LEGO studs (x direction)
+    // H = 25 // half height in LEGO studs (y direction)
+    // R = 1 // half width of roads in studs
+    // B = 4 // one block is 4 LEGO studs
+
+    var links = [];
+    links.push([[-W+R, H-R,0],[ W-R, H-R,0]]); // top boundary
+    links.push([[-W+R,-H+R,0],[ W-R,-H+R,0]]); // bottom boundary
+    links.push([[-W+R,-H+R,0],[-W+R, H-R,0]]); // left boundary
+    links.push([[ W-R,-H+R,0],[ W-R, H-R,0]]); // right boundary
+
+    for ( var i=-W+B+3*R;i<W-R;i+=B+2*R ) {
+        links.push([[i, H-R,0],[i,-H+R,0]]); // vertical roads
+    }
+    for ( var i=-H+B+3*R;i<H-R;i+=B+2*R ) {
+        links.push([[W-R,i,0],[-W+R,i,0]]); // horizontal roads
+    }
+    // var links = [[[-12,-12,0],[-12, 12,0]],
+    //              [[-12,-12,0],[ 12,-12,0]],
+    //              [[ 12, 12,0],[ 12,-12,0]],
+    //              [[ 12, 12,0],[-12, 12,0]],
+    //              [[-12,  0,0],[ 12,  0,0]], // horizontal middle
+    //              [[  0,-12,0],[  0, 12,0]], // vertical middle
+    //          ];
+    return links
+}
+
 import * as THREE from './node_modules/three/build/three.module.js';
 import { LineGeometry } from './node_modules/three/examples/jsm/lines/LineGeometry.js';
 import { LineMaterial } from './node_modules/three/examples/jsm/lines/LineMaterial.js';
@@ -91,18 +120,21 @@ import { Line2 } from './node_modules/three/examples/jsm/lines/Line2.js';
 import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
 // import { nelderMead } from './node_modules/fmin/src/nelderMead.js'
 
+var W = 25/2. // half width in LEGO studs (x direction)
+var H = 25/2. // half height in LEGO studs (y direction)
+var road_width = 2/2 // width of roads in studs
+var block_length = 4 // one block is 4 LEGO studs
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
-camera.position.z = 20;
+camera.position.z = 2*W;
 
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 // var controls = new OrbitControls( camera, renderer.domElement );
 
-
-
-var geometry = new THREE.PlaneGeometry( 25, 25, 32 );
+var geometry = new THREE.PlaneGeometry( 2*W, 2*H);
 var base_material = new THREE.MeshBasicMaterial( {color: 0xffffff, side: THREE.DoubleSide} );
 var base_plane = new THREE.Mesh( geometry, base_material );
 scene.add( base_plane );
@@ -121,13 +153,7 @@ var line_material = new LineMaterial( {
 } );
 line_material.defines.USE_DASH = ""; // enables dashing
 
-var links = [[[-12,-12,0],[-12, 12,0]],
-             [[-12,-12,0],[ 12,-12,0]],
-             [[ 12, 12,0],[ 12,-12,0]],
-             [[ 12, 12,0],[-12, 12,0]],
-             [[-12,  0,0],[ 12,  0,0]], // horizontal middle
-             [[  0,-12,0],[  0, 12,0]], // vertical middle
-         ];
+var links = generate_regular_roads(W,H,road_width,block_length)
 
 // locate_domain()
 
@@ -135,7 +161,7 @@ scene.background = new THREE.Color( 0xFF0000 );
 init();
 
 function init() {
-    add_road_network(links)
+    add_road_network(links,road_width)
     // calibrate_camera();
 
     window.addEventListener( 'resize', onWindowResize, false );
