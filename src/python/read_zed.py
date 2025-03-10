cb8aa2923dabc08a11dababcd92cf1c5a4d94f7b
Benjy Marks <benjy.marks@sydney.edu.au>
2025-03-10 16:54:47 +1100

    now reading camera and sending data

--- src/python/read_zed.py
+++ src/python/read_zed.py
@@ -21,6 +21,7 @@
 import pyzed.sl as sl
 import numpy as np
 import matplotlib.pyplot as plt
+# import scipy.ndimage
 import sys
 import cv2
 import requests
@@ -29,7 +30,7 @@ import traceback
 import time
 
 
-p = json5.loads(open("../../params.json5").read())
+p = json5.loads(open("params.json5").read())
 debug = False
 # debug = True
 # server_url = "http://localhost:5000/"
@@ -40,7 +41,7 @@ def order_points(pts):
     # such that the first entry in the list is the top-left,
     # the second entry is the top-right, the third is the
     # bottom-right, and the fourth is the bottom-left
-    rect = np.zeros((4, 2), dtype="float32")
+    rect = np.zeros((4, 2), dtype=np.float32)
     # the top-left point will have the smallest sum, whereas
     # the bottom-right point will have the largest sum
     s = pts.sum(axis=1)
@@ -56,46 +57,87 @@ def order_points(pts):
     return rect
 
 
-def four_point_transform(image, pts, grid):
+def four_point_transform(image, pts, grid, debug=False):
     # obtain a consistent order of the points and unpack them
     # individually
-    rect = order_points(pts)
-
-    if grid == "native":
-        (tl, tr, br, bl) = rect
-        # compute the width of the new image, which will be the
-        # maximum distance between bottom-right and bottom-left
-        # x-coordiates or the top-right and top-left x-coordinates
-        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-        # compute the height of the new image, which will be the
-        # maximum distance between the top-right and bottom-right
-        # y-coordinates or the top-left and bottom-left y-coordinates
-        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-        maxWidth = max(int(widthA), int(widthB))
-        maxHeight = max(int(heightA), int(heightB))
-    else:
-        maxWidth = grid[0]
-        maxHeight = grid[1]
+    # rect = order_points(pts)
+    rect = np.float32(pts)
+    
+    # if grid == "native":
+    #     (tl, tr, br, bl) = rect
+    #     # compute the width of the new image, which will be the
+    #     # maximum distance between bottom-right and bottom-left
+    #     # x-coordiates or the top-right and top-left x-coordinates
+    #     widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+    #     widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+    #     # compute the height of the new image, which will be the
+    #     # maximum distance between the top-right and bottom-right
+    #     # y-coordinates or the top-left and bottom-left y-coordinates
+    #     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+    #     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+    #     maxWidth = max(int(widthA), int(widthB))
+    #     maxHeight = max(int(heightA), int(heightB))
+    # else:
+    maxWidth = grid[0]
+    maxHeight = grid[1]
 
     # now that we have the dimensions of the new image, construct
     # the set of destination points to obtain a "birds eye view",
     # (i.e. top-down view) of the image, again specifying points
     # in the top-left, top-right, bottom-right, and bottom-left
     # order
-    dst = np.array(
-        [[0, 0], [maxWidth - 1, 0], [maxWidth - 1, maxHeight - 1], [0, maxHeight - 1]],
-        dtype="float32",
+    dst = np.float32(
+        [[0, 0], [maxWidth - 1, 0], [maxWidth - 1, maxHeight - 1], [0, maxHeight - 1]]
     )
     # compute the perspective transform matrix and then apply it
-    M = cv2.getPerspectiveTransform(rect, dst)
-    warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+    
+    
+    # Assuming rect and dst are defined somewhere in your code
+    rect = np.array(rect, dtype=np.float32)
+    dst = np.array(dst, dtype=np.float32)
+
+    # Ensure the arrays are contiguous
+    # rect = np.ascontiguousarray(rect)
+    # dst = np.ascontiguousarray(dst)
+
+    # Convert to UMat
+    # rect_umat = cv2.UMat(rect)
+    # dst_umat = cv2.UMat(dst)
+
+
+    # try:
+    #     M = cv2.getPerspectiveTransform(rect, dst)
+    #     print("Transformation matrix M:", M)
+    # except cv2.error as e:
+    #     print("OpenCV error:", e)
+    # except Exception as e:
+    #     print("Error:", e)
+
+
+    #M = cv2.getPerspectiveTransform(rect, dst)
+    #warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
     # return the warped image
-    return warped
+    im = np.array(image)
+    crop = im[p["corners"][0][0]:p["corners"][2][0], p["corners"][0][1]:p["corners"][1][1]]
+    dx = crop.shape[1] / maxWidth
+    dy = crop.shape[0] / maxHeight
+
+    if len(crop.shape) == 2:
+        colours = np.zeros([maxWidth, maxHeight])
+    else:
+        colours = np.zeros([maxWidth, maxHeight, crop.shape[2]])
+    for i in range(maxWidth):
+        for j in range(maxHeight):
+            x = int(i * dx)
+            y = int(j * dy)
+            colours[i, j] = np.median(crop[y:y+int(dy), x:x+int(dx)], axis=(0, 1))
+    if debug:
+        plt.imsave("crop.png", crop)
+
+    return colours
 
 
-def get_corners(zed, runtime_parameters, N, p):
+def get_corners(zed, runtime_parameters, N, p, debug=False):
     i = 0
     image = sl.Mat()
     out = np.zeros([N, 4, 2])
@@ -176,6 +218,31 @@ def get_corners(zed, runtime_parameters, N, p):
     # print(averaged_corners)
     # np.savetxt('corners.txt',averaged_corners)
     return averaged_corners
+
+def get_corners_hardcoded(zed, runtime_parameters, N, p, debug=False):
+    if debug:
+        image = sl.Mat()
+        if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
+            # Retrieve left image
+            zed.retrieve_image(image, sl.VIEW.LEFT)
+            im = np.array(image.get_data())
+            print(im.shape)
+            print(im.dtype)
+
+            # Draw markers for each corner in p["corners"]
+            for corner in p["corners"]:
+                # Assuming each corner is a tuple or list with (x, y) coordinates
+                # x, y = corner
+                # print(x,y)
+                im[corner[0]-2:corner[0]+3,corner[1]-2:corner[1]+3,:] = 255
+                # Draw a red circle at each corner (radius 5 pixels)
+                # cv2.circle(im[:,:,:3], (int(x), int(y)), 5, (0, 0, 255), -1)
+            plt.imsave("corners.png", im[:,:,:3])
+
+            # Display the image with markers
+            # cv2.imshow('frame with corners', im)
+
+    return p["corners"]
 
 
 def get_warped_data(image, depths, pts, grid, colour, height):
@@ -265,11 +332,11 @@ def get_zed_frame(zed, runtime_parameters, image, depth, corners, grid, colours,
 def main():
     zed, runtime_parameters = initialise_camera()
 
-    p = json5.loads(open("../../params.json5").read())
+    p = json5.loads(open("params.json5").read())
 
     grid = [
-        p.W,
-        p.H,
+        p["W"],
+        p["H"],
     ]  # how many lego studs are available in horizontal and vertical direction
     # grid = 'native' # get the best image possible
 
@@ -279,8 +346,9 @@ def main():
     depth = sl.Mat()
 
     try:
-        corners = get_corners(zed, runtime_parameters, 3, p)
-    except:
+        # corners = get_corners(zed, runtime_parameters, 3, p)
+        corners = get_corners_hardcoded(zed, runtime_parameters, 3, p)
+    except Exception as e:
         print("Failed to find corners, quitting gracefully. Got the exception:")
         traceback.print_exc(file=sys.stdout)
         zed.close()
@@ -293,10 +361,12 @@ def main():
                 zed, runtime_parameters, image, depth, corners, grid, colours, heights
             )
 
+            # print(heights.mean())
+
             # remove offset to plane from heights and convert to studs
             # base_offset = (height[0,0] + height[0,-1] + height[-1,0] + height[-1,-1])/4. + 8e-3
-            lego = p.base_offset - heights  # pretty bloody unlikely to work
-            lego /= p.brick_height * 1e-3  # 9.6 mm per stud
+            lego = p["base_offset"] - heights  # pretty bloody unlikely to work
+            lego /= p["brick_height"] * 1e-3  # 9.6 mm per stud
             # print(np.around(lego))
             lego[~np.isfinite(lego)] = 0.0
             lego = lego.astype(np.int64)
@@ -304,18 +374,18 @@ def main():
             # print(lego.tolist()[0])
             # sending post request and saving response as response object
             r = requests.post(
-                url=p.server_url + "/post_zed_data_to_server",
+                url=p["imac_url"] + "/post_zed_data_to_server",
                 data={
                     "depths": json5.dumps(lego.tolist()),
                     "colours": json5.dumps(colours.astype(np.int64).tolist()),
                 },
             )
             print("The server responded with: " + r.text)
-        except:
+        except Exception as e:
             print("Failed to get frame, quitting gracefully. Got the exception:")
             traceback.print_exc(file=sys.stdout)
-            zed.close()
-            exit(1)
+            # zed.close()
+            # exit(1)
 
     # Close the camera
     zed.close()
